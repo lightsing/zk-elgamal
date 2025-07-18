5523d3732a2f4a3f2cba47f77563f3d04614fc0a
lightsing <light.tsing@gmail.com>
2025-07-18 10:58:43 +0800

    run many

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,43 +1,41 @@
 #![no_main]
 
+use std::hint::black_box;
 use elgamal_lib::*;
 
 sp1_zkvm::entrypoint!(main);
 
 pub fn main() {
+    let repetitions = sp1_zkvm::io::read::<u32>();
     let exec_mode = sp1_zkvm::io::read::<ExecMode>();
     let mode = sp1_zkvm::io::read::<Mode>();
 
     match mode {
-        Mode::Encrypt => encrypt(exec_mode),
-        Mode::Decrypt => decrypt(exec_mode),
+        Mode::Encrypt => encrypt(exec_mode, repetitions),
+        Mode::Decrypt => decrypt(exec_mode, repetitions),
     }
 }
 
-fn encrypt(exec_mode: ExecMode) {
+fn encrypt(exec_mode: ExecMode, repetitions: u32) {
     let pk = sp1_zkvm::io::read::<PublicKey>();
     let nonce = sp1_zkvm::io::read::<Scalar>();
     let message = sp1_zkvm::io::read::<RistrettoPoint>();
 
-    let mut ciphertext = Ciphertext::default();
-
     if exec_mode == ExecMode::All {
-        ciphertext = pk.encrypt(nonce, &message);
+        for _ in 0..repetitions {
+            black_box(black_box(pk).encrypt(black_box(nonce), black_box(&message)));
+        }
     }
-
-    sp1_zkvm::io::commit(&ciphertext);
 }
 
-fn decrypt(exec_mode: ExecMode) {
+fn decrypt(exec_mode: ExecMode, repetitions: u32) {
     let sk = sp1_zkvm::io::read::<SecretKey>();
     let ciphertext = sp1_zkvm::io::read::<Ciphertext>();
 
-    let mut decrypted_message = RistrettoPoint::default();
-
     if exec_mode == ExecMode::All {
-        decrypted_message = sk.decrypt(&ciphertext);
+        for _ in 0..repetitions {
+            black_box(black_box(&sk).decrypt(black_box(&ciphertext)));
+        }
     }
-
-    sp1_zkvm::io::commit(&decrypted_message);
 }
 
--- script/src/main.rs
+++ script/src/main.rs
@@ -1,9 +1,17 @@
 use std::env;
 use rand::thread_rng;
-use sp1_sdk::{SP1Stdin, include_elf, CpuProver, Prover};
+use sp1_sdk::{SP1Stdin, include_elf, CpuProver, Prover, SP1ProvingKey, SP1VerifyingKey};
 use elgamal_lib::*;
 
 pub const ELF: &[u8] = include_elf!("elgamal-program");
+
+struct Case {
+    secret_key: SecretKey,
+    public_key: PublicKey,
+    nonce: Scalar,
+    message: RistrettoPoint,
+    ciphertext: Ciphertext,
+}
 
 fn main() {
     sp1_sdk::utils::setup_logger();
@@ -13,141 +21,125 @@ fn main() {
 
     let mut rng = thread_rng();
 
-    // let mut results = vec![];
-    //
-    // for _ in 0..1000 {
     let secret_key = SecretKey::new(Scalar::random(&mut rng));
     let public_key = PublicKey::from(&secret_key);
     let nonce = Scalar::random(&mut rng);
     let message = RistrettoPoint::random(&mut rng);
     let ciphertext = public_key.encrypt(nonce, &message);
 
-    println!("[+] Running encrypt:");
+    let case = Case {
+        secret_key,
+        public_key,
+        nonce,
+        message,
+        ciphertext,
+    };
 
-    #[cfg(feature = "profiling")]
-    unsafe {
-        env::set_var("TRACE_FILE", "encrypt.json");
+    run(
+        &client,
+        &proving_key,
+        &verifying_key,
+        &case,
+        1000, // number of repetitions
+        Mode::Encrypt,
+    );
+
+    run(
+        &client,
+        &proving_key,
+        &verifying_key,
+        &case,
+        1000, // number of repetitions
+        Mode::Decrypt,
+    );
+}
+
+fn run(
+    client: &CpuProver,
+    proving_key: &SP1ProvingKey,
+    verifying_key: &SP1VerifyingKey,
+    case: &Case,
+    repetitions: u32,
+    mode: Mode,
+){
+    match mode {
+        Mode::Encrypt => println!("[+] Running encrypt:"),
+        Mode::Decrypt => println!("[+] Running decrypt:"),
+    }
+
+    if cfg!(feature = "profiling") {
+        env::set_var("TRACE_FILE", match mode {
+            Mode::Encrypt => format!("encrypt-{repetitions}.json"),
+            Mode::Decrypt => format!("decrypt-{repetitions}.json"),
+        });
     }
 
     let mut stdin = SP1Stdin::new();
+    stdin.write(&repetitions);
     stdin.write(&ExecMode::All);
+    match mode {
+        Mode::Encrypt => write_encrypt_stdin(case, &mut stdin),
+        Mode::Decrypt => write_decrypt_stdin(case, &mut stdin),
+    }
+
+    let (_, report) = client.execute(&ELF, &stdin).run().unwrap();
+    let total_instruction_count = report.total_instruction_count();
+    println!("- Total Instructions: {total_instruction_count}");
+
+    if !cfg!(feature = "profiling") {
+        let now = std::time::Instant::now();
+        let proof = client.prove(&proving_key, &stdin).compressed().run().unwrap();
+        let total_proving_time = now.elapsed();
+        // sanity check
+        client.verify(&proof, &verifying_key).unwrap();
+
+        // run baseline
+        let mut stdin = SP1Stdin::new();
+        stdin.write(&ExecMode::Baseline);
+        match mode {
+            Mode::Encrypt => write_encrypt_stdin(case, &mut stdin),
+            Mode::Decrypt => write_decrypt_stdin(case, &mut stdin),
+        }
+        let (_, report) = client.execute(&ELF, &stdin).run().unwrap();
+        let baseline_instruction_count = report.total_instruction_count();
+        let net_instruction_count = total_instruction_count - baseline_instruction_count;
+        let net_instruction_per_operation = net_instruction_count as f64 / repetitions as f64;
+
+        let now = std::time::Instant::now();
+        let proof = client.prove(&proving_key, &stdin).compressed().run().unwrap();
+        let baseline_proving_time = now.elapsed();
+        let net_proving_time = total_proving_time - baseline_proving_time;
+        let net_proving_time_per_operation = net_proving_time / repetitions;
+
+        // sanity check
+        client.verify(&proof, &verifying_key).unwrap();
+
+
+        println!("- Total Proving Time: {total_proving_time:?}");
+        println!("- Baseline Instructions: {baseline_instruction_count}");
+        println!("- Baseline Proving Time: {baseline_proving_time:?}");
+        println!("- Net Instructions: {net_instruction_count}");
+        println!("- Net Proving Time: {net_proving_time:?}");
+        println!("- Net Instructions per operation: {net_instruction_per_operation:.2}");
+        println!("- Net Proving Time per operation: {net_proving_time_per_operation:?}");
+    }
+}
+
+fn write_encrypt_stdin(
+    case: &Case,
+    stdin: &mut SP1Stdin,
+) {
     stdin.write(&Mode::Encrypt);
-    stdin.write(&public_key);
-    stdin.write(&nonce);
-    stdin.write(&message);
-    let (mut public_values, report) = client.execute(&ELF, &stdin).run().unwrap();
-    let enc_total_instruction_count = report.total_instruction_count();
-    println!("- Total Instructions: {enc_total_instruction_count}");
-    let ct_vm: Ciphertext = public_values.read();
-    assert_eq!(ct_vm, ciphertext);
+    stdin.write(&case.public_key);
+    stdin.write(&case.nonce);
+    stdin.write(&case.message);
+}
 
-    #[cfg(not(feature = "profiling"))]
-    {
-        let now = std::time::Instant::now();
-        let proof = client.prove(&proving_key, &stdin).compressed().run().unwrap();
-        let enc_total_proving_time = now.elapsed();
-        println!("- Total Proving Time: {enc_total_proving_time:?}");
-        client.verify(&proof, &verifying_key).unwrap();
-
-        let mut stdin = SP1Stdin::new();
-        stdin.write(&ExecMode::Baseline);
-        stdin.write(&Mode::Encrypt);
-        stdin.write(&public_key);
-        stdin.write(&nonce);
-        stdin.write(&message);
-        let (_, report) = client.execute(&ELF, &stdin).run().unwrap();
-        let enc_net_instruction_count = enc_total_instruction_count - report.total_instruction_count();
-        println!("- Net Instructions: {enc_net_instruction_count}", );
-
-        let now = std::time::Instant::now();
-        let proof = client.prove(&proving_key, &stdin).compressed().run().unwrap();
-        let enc_net_proving_time = now.elapsed() - enc_total_proving_time;
-        println!("- Net Proving Time: {enc_net_proving_time:?}");
-        client.verify(&proof, &verifying_key).unwrap();
-    }
-
-
-    println!("[+] Running decrypt:");
-
-    #[cfg(feature = "profiling")]
-    unsafe {
-        env::set_var("TRACE_FILE", "decrypt.json");
-    }
-
-    let mut stdin = SP1Stdin::new();
-    stdin.write(&ExecMode::All);
+fn write_decrypt_stdin(
+    case: &Case,
+    stdin: &mut SP1Stdin,
+) {
     stdin.write(&Mode::Decrypt);
-    stdin.write(&secret_key);
-    stdin.write(&ciphertext);
-    let (mut public_values, report) = client.execute(&ELF, &stdin).run().unwrap();
-    let dec_total_instruction_count = report.total_instruction_count();
-    println!("- Total Instructions: {dec_total_instruction_count}");
-    let decrypted_message_vm: RistrettoPoint = public_values.read();
-    assert_eq!(decrypted_message_vm, secret_key.decrypt(&ciphertext));
-
-    #[cfg(not(feature = "profiling"))]
-    {
-        let now = std::time::Instant::now();
-        let proof = client.prove(&proving_key, &stdin).compressed().run().unwrap();
-        let dec_total_proving_time = now.elapsed();
-        println!("- Total Proving Time: {dec_total_proving_time:?}");
-        client.verify(&proof, &verifying_key).unwrap();
-
-        let mut stdin = SP1Stdin::new();
-        stdin.write(&ExecMode::Baseline);
-        stdin.write(&Mode::Decrypt);
-        stdin.write(&secret_key);
-        stdin.write(&ciphertext);
-        let (_, report) = client.execute(&ELF, &stdin).run().unwrap();
-        let dec_net_instruction_count = dec_total_instruction_count - report.total_instruction_count();
-        println!("- Net Instructions: {dec_net_instruction_count}");
-
-        let now = std::time::Instant::now();
-        let proof = client.prove(&proving_key, &stdin).compressed().run().unwrap();
-        let dec_net_proving_time = now.elapsed() - dec_total_proving_time;
-        println!("- Net Proving Time: {dec_net_proving_time:?}");
-        client.verify(&proof, &verifying_key).unwrap();
-    }
-    //     results.push((
-    //         enc_total_instruction_count,
-    //         enc_net_instruction_count,
-    //         enc_total_proving_time,
-    //         dec_total_instruction_count,
-    //         dec_net_instruction_count,
-    //         dec_total_proving_time,
-    //     ));
-    // }
-    //
-
-    // // Print avg
-    //
-    // let len = results.len() as u64;
-    //
-    // let (
-    //     enc_total_instruction_count,
-    //     enc_net_instruction_count,
-    //     enc_total_proving_time,
-    //     dec_total_instruction_count,
-    //     dec_net_instruction_count,
-    //     dec_total_proving_time,
-    // ) = results
-    //     .into_iter()
-    //     .reduce(
-    //         |acc, item| (
-    //             acc.0 + item.0,
-    //             acc.1 + item.1,
-    //             acc.2 + item.2,
-    //             acc.3 + item.3,
-    //             acc.4 + item.4,
-    //             acc.5 + item.5,
-    //         )
-    //     ).unwrap();
-    //
-    // println!("[+] Results:");
-    // println!("- Avg Encrypt Total Instructions: {}", enc_total_instruction_count / len);
-    // println!("- Avg Encrypt Net Instructions: {}", enc_net_instruction_count / len);
-    // println!("- Avg Encrypt Total Proving Time: {} s", enc_total_proving_time.as_secs() / len);
-    // println!("- Avg Decrypt Total Instructions: {}", dec_total_instruction_count / len);
-    // println!("- Avg Decrypt Net Instructions: {}", dec_net_instruction_count / len);
-    // println!("- Avg Decrypt Total Proving Time: {} s", dec_total_proving_time.as_secs() / len);
+    stdin.write(&case.secret_key);
+    stdin.write(&case.ciphertext);
 }
